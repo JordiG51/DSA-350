93bfccfcf5e47a5fa8070b4e42cdf63ce9c43a7d
Jordi <reddepth825@gmail.com>
2026-02-12 10:21:35 -0500

    fist commit

--- planet_classes.py
+++ planet_classes.py
@@ -13,8 +13,7 @@ class planet():
         self.name = name
         self.moon_list = []
 class moon():
-    def __init__(self,name, color = "white", radius = 1,
-                     tidally_locked=False, planet_companion =None):
+    def __init__(self,name, color = "white", radius = 1, tidally_locked=False, planet_companion =None):
         self.name = name
         self.radius = radius
         self.tidally_locked = tidally_locked
@@ -31,3 +30,18 @@ def print_largest(pl): #I did this whole thing for you - it selects the largest 
             if largest.radius < moon.radius: largest = moon      
     if largest is not None:
         print(f"The largest moon of {pl.name} is {largest.name}")
+
+planet_d = dict() #key: name of planet. value: planet object
+for index, row in df.iterrows(): #get all the planets first
+    if row['isPlanet'] is True:
+        p = planet(name = index, radius =row['meanRadius'])
+        planet_d[index] =p
+
+moon_d = dict() #key: name of moon. value: moon object
+for index, row in df.iterrows(): #get all the planets first
+    if row['isPlanet'] is False:
+        planet_name =row['orbits']
+        if planet_name in planet_d:
+            host_planet =planet_d[planet_name]
+            m = moon(name = index, radius =row['meandRadius']
+
